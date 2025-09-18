259ec9f570a1206b4c17d7796e4f4ca0c20d273f
Blynchik <vadimsovetnikov@mail.ru>
2025-09-18 19:48:09 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "dev.blynchik"
+version = "0.0.1-SNAPSHOT"
+description = "Telegram Quest Bot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.telegram:telegrambots-spring-boot-starter:6.9.7.1")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/dev/blynchik/quest_bot/QuestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.blynchik.quest_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
